827a95e7ad2667dc1c98cadf3bea5ee56daad802
Huang <kk20210112@gmail.com>
2022-10-04 18:50:07 +0800

    user_avatar_path

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,11 +3,17 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/inertia-react';
 
 export default function Dashboard(props) {
+    console.log(props)
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <div className="flex align-content justify-content">
+                    <img src={props.auth.user.user_avatar_path} alt={props.auth.user.name} width="36" height="36"></img>
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">{props.auth.user.name}</h2>
+                </div>
+            }
         >
             <Head title="Dashboard" />
 
